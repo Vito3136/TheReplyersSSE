e86e358a0cc441d15b0dae75e5a20e6d98901eac
Vito3136 <vito3136@gmail.com>
2025-07-01 12:28:40 +0200

    - issue fixed

--- src/main/java/com/example/vulnapp/model/User.java
+++ src/main/java/com/example/vulnapp/model/User.java
@@ -1,9 +1,14 @@
 package com.example.vulnapp.model;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
     private long id;
     private String username;
-    private String password;
+    private transient String password;
 
     public long getId() { return id; }
     public void setId(long id) { this.id = id; }
